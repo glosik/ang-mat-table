79f8aa00054e13f6f29e30f9fa6c32c6fe9295c7
iuri Strelet <istrelet@411.ca>
2018-04-01 16:05:51 -0400

    04 : add logic

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import * as players from './shorthanded.json';
+
+export interface Player {
+	'gamesPlayed': number;
+	'playerBirthCity': string;
+	'playerBirthCountry': string;
+	'playerBirthDate': string;
+	'playerBirthStateProvince': string;
+	'playerDraftOverallPickNo': number;
+	'playerDraftRoundNo': number;
+	'playerDraftYear': number;
+	'playerFirstName': string;
+	'playerHeight': number;
+	'playerId': number;
+	'playerInHockeyHof': number;
+	'playerIsActive': number;
+	'playerLastName': string;
+	'playerName': string;
+	'playerNationality': string;
+	'playerPositionCode': string;
+	'playerShootsCatches': string;
+	'playerTeamsPlayedFor': string;
+	'playerWeight': number;
+	'seasonId': number;
+	'shAssists': number;
+	'shBlockedShots': number;
+	'shFaceoffsLost': number;
+	'shFaceoffsWon': number;
+	'shGiveaways': number;
+	'shGoals': number;
+	'shHits': number;
+	'shMissedShots': number;
+	'shPoints': number;
+	'shShots': number;
+	'shTakeaways': number;
+	'shTimeOnIce': number;
+}
+
+@Component({
+	selector: 'app-table',
+	templateUrl: './table.component.html',
+	styleUrls: ['./table.component.css']
+})
+
+export class TableComponent implements AfterViewInit {
+	displayedColumns = ['playerName', 'shTimeOnIce'];
+	shorthanded = players;
+	// TOD: fix Property 'data' does not exist on type 'typeof '*.json''.
+	PLAYER_DATA: Player[] = this.shorthanded.data;
+	dataSource = new MatTableDataSource(this.PLAYER_DATA);
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
+
+	ngAfterViewInit() {
+		this.dataSource.paginator = this.paginator;
+		this.dataSource.sort = this.sort;
+	}
+
+	applyFilter(filterValue: string) {
+		filterValue = filterValue.trim(); // Remove whitespace
+		filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+		this.dataSource.filter = filterValue;
+	}
+}
